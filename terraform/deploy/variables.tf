b3c58f26ce2ef804772deb23da6ee47987812bf1
AleksandrLoskutov <107686705+AleksandrLoskutov@users.noreply.github.com>
2022-12-01 12:04:17 +0300

    Create variables.tf

---
+++ terraform/deploy/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_family_image" {
+  description = "Instance image"
+  type        = string
+  default     = "ubuntu-2204-lts"
+}
+
+variable "ssh_credentials" {
+  description = "Credentials for connect to instances"
+  type        = object({
+    user        = string
+    private_key = string
+    pub_key     = string
+  })
+  default     = {
+    user        = "ubuntu"
+    private_key = "~/.ssh/id_***"
+    pub_key     = "~/.ssh/id_***.pub"
+  }
+}
