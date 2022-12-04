27bc89bb5e6e239fbbb437dec9c6456b931cd985
AleksandrLoskutov <107686705+AleksandrLoskutov@users.noreply.github.com>
2022-12-04 11:41:30 +0300

    Create provisioning.sh

---
+++ terraform/deploy/provisioning.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+server_ip=$1
+master_ip=$2
+worker_ip=$3
+
+while : ; do
+ssh -q -o "StrictHostKeyChecking=no" -o "ConnectTimeout=10" ubuntu@$master_ip exit && ssh -q -o "StrictHostKeyChecking=no" -o "ConnectTimeout=10" ubuntu@$worker_ip exit && ssh -q -o "StrictHostKeyChecking=no" -o "ConnectTimeout=10" ubuntu@$server_ip exit
+[ $? -eq 0 ]&& break
+echo "Waiting for nodes initialization..."
+done
+
+echo "CONNECTING TO NODES FOR PROVISIONING..."
+ansible-playbook -i inventory playbook.yml
